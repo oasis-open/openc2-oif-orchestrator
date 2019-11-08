4a2c7b4c45082200527a9eda37a5176077aff379
Jerome Czachor <jerome.czachor@g2-inc.com>
2019-11-08 20:56:10 +0000

    Update from repo GUI - 2019-11-08 20:56:10 UTC [CI Skip]

--- orchestrator/gui/client/src/components/device/index.js
+++ orchestrator/gui/client/src/components/device/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Helmet } from 'react-helmet-async';
+import { Helmet } from 'react-helmet-async'
 
 import {
   Button,
--- orchestrator/gui/client/src/components/static/error.js
+++ orchestrator/gui/client/src/components/static/error.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Helmet } from 'react-helmet-async';
+import { Helmet } from 'react-helmet-async'
 import { toast } from 'react-toastify'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
