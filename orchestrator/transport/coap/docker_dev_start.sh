dc12deb5e91b7018e1b20318eb9c38ce79593507
jerome.czachor <jerome.czachor@hii-tsd.com>
2020-08-25 16:55:07 +0000

    Update from repo Commit - 2020-08-25 16:55:07 UTC [CI Skip]

--- orchestrator/transport/coap/docker_dev_start.sh
+++ orchestrator/transport/coap/docker_dev_start.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+cd /opt/transport
 
 echo "Running COAP Transport Module."
 dockerize -wait tcp://$QUEUE_HOST:$QUEUE_PORT -timeout 30s
 
 echo "Starting CoAP Server"
-python3 -u coap_server.py &
+python3 -u ./COAP/coap_server.py &
 status=$?
 if [[ $status -ne 0 ]]; then
   echo "Failed to start CoAP Server: $status"
@@ -12,7 +13,7 @@ if [[ $status -ne 0 ]]; then
 fi
 
 echo "Starting CoAP Client"
-python3 -u coap_client.py
+python3 -u ./COAP/coap_client.py
 status=$?
 if [[ $status -ne 0 ]]; then
   echo "Failed to start CoAP Client: $status"
