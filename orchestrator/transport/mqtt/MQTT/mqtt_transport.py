256ae3153e2bb038386c317bd4e6931ef85bc36e
jerome.czachor <jerome.czachor@hii-tsd.com>
2022-05-23 12:40:35 +0000

    Update from repo MQTT - 2022-05-23 12:40:35 UTC [CI Skip]

--- orchestrator/transport/mqtt/MQTT/mqtt_transport.py
+++ orchestrator/transport/mqtt/MQTT/mqtt_transport.py
@@ -1,14 +1,32 @@
 # mqtt_transport.py
 import os
+import uuid
+import etcd
 
+from time import sleep
 from sb_utils import Consumer, EtcdCache, safe_cast
 from mqtt_connections import ClientsMQTT, send_mqtt
 
 if __name__ == '__main__':
+    # Get Orchestrator ID
+    # Connect to Etcd
+    etcd_client = etcd.Client(
+        host=os.environ.get("ETCD_HOST", "localhost"),
+        port=safe_cast(os.environ.get("ETCD_PORT", 2379), int, 2379)
+    )
+
+    # Generate/Get 23 character Device ID from ...
+    while True:
+        try:
+            orc_id = str(uuid.UUID(etcd_client.read('/device/id').value)).replace("-", "")[:16]
+            break
+        except etcd.EtcdKeyNotFound:
+            sleep(1)
+
     # Initialize responses object
     mqtt_conns = ClientsMQTT(
         # TODO: add orc_id to client_id ??
-        # client_id=
+        client_id=f"oif-orc-{orc_id}",
         # Add subscription - oc2/rsp/PRODUCER_ID
         topics=['+/+/oc2/rsp', '+/oc2/rsp', 'oc2/rsp'],
         debug=True
@@ -35,7 +53,9 @@ if __name__ == '__main__':
             callbacks=[send_mqtt],
             debug=True
         )
+        consumer.join()
     except Exception as err:
         print(f"Consumer Error: {err}")
         consumer.shutdown()
-        mqtt_conns.shutdown()
+    mqtt_conns.shutdown()
+    transport_cache.shutdown()
